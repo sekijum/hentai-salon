0bd205f0f7e9b48b3e56784ea6bc5d2a57daf72b
sekijum <Jumpei0910@icloud.com>
2024-07-02 01:35:20 +0900

    fix: 無限スクロール

--- packages/client/components/MediaGallery.vue
+++ packages/client/components/MediaGallery.vue
@@ -1,28 +1,46 @@
 <template>
-  <v-row class="pa-0 ma-0">
-    <v-col v-for="n in 1000" :key="n" class="d-flex child-flex pa-0 ma-0" cols="4">
-      <v-img
-        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-        :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-        aspect-ratio="1"
-        class="bg-grey-lighten-2"
-        cover
-        @click="router.push('/comments/1')"
-      >
-        <template v-slot:placeholder>
-          <v-row align="center" class="fill-height ma-0" justify="center">
-            <v-progress-circular color="grey-lighten-5" indeterminate></v-progress-circular>
-          </v-row>
-        </template>
-      </v-img>
-    </v-col>
-  </v-row>
+  <v-infinite-scroll height="100%" :items="items" :onLoad="load">
+    <v-row class="pa-0 ma-0">
+      <v-col v-for="n in items" :key="n" class="d-flex child-flex pa-0 ma-0" cols="4">
+        <v-img
+          :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+          :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+          aspect-ratio="1"
+          class="bg-grey-lighten-2"
+          cover
+          @click="router.push('/comments/1')"
+        >
+          <template v-slot:placeholder>
+            <v-row align="center" class="fill-height ma-0" justify="center">
+              <v-progress-circular color="grey-lighten-5" indeterminate></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </v-col>
+    </v-row>
+  </v-infinite-scroll>
 </template>
 
 <script setup lang="ts">
-import { useRouter, useRoute } from 'vue-router';
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
 const router = useRouter();
+const items = ref(Array.from({ length: 30 }, (k, v) => v + 1));
+
+async function api() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(Array.from({ length: 10 }, (k, v) => v + items.value.at(-1) + 1));
+    }, 1000);
+  });
+}
+
+async function load({ done }) {
+  const res = await api();
+  items.value.push(...res);
+  done('ok');
+}
 </script>
 
 <style scoped>
