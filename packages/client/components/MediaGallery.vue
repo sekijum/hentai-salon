0a92d60371ec9128b36bb507f46b68b9d154a5d9
sekijum <Jumpei0910@icloud.com>
2024-07-03 23:45:43 +0900

    fix: videoアイコン対応

--- packages/client/components/MediaGallery.vue
+++ packages/client/components/MediaGallery.vue
@@ -1,20 +1,21 @@
 <template>
   <v-infinite-scroll height="100%" :items="items" :onLoad="load">
     <v-row class="pa-0 ma-0">
-      <v-col v-for="n in items" :key="n" class="d-flex child-flex pa-0 ma-0" cols="6">
+      <v-col v-for="item in items" :key="item.id" class="d-flex child-flex pa-0 ma-0" cols="6">
         <v-img
-          :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-          :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+          :lazy-src="item.lazySrc"
+          :src="item.src"
           aspect-ratio="1"
           class="bg-grey-lighten-2"
           cover
-          @click="navigateToExternalPath(`/comments/${n}`)"
+          @click="navigateToExternalPath(`/comments/${item.id}`)"
         >
           <template v-slot:placeholder>
             <v-row align="center" class="fill-height ma-0" justify="center">
               <v-progress-circular color="grey-lighten-5" indeterminate></v-progress-circular>
             </v-row>
           </template>
+          <v-icon v-if="item.type === 'video/mp4'" size="60" class="play-icon">mdi-play-circle</v-icon>
         </v-img>
       </v-col>
     </v-row>
@@ -24,12 +25,37 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const items = ref(Array.from({ length: 30 }, (k, v) => v + 1));
+const items = ref([
+  ...Array.from({ length: 20 }, (k, v) => ({
+    id: v + 1,
+    type: 'image',
+    lazySrc: `https://picsum.photos/10/6?image=${(v + 1) * 5 + 10}`,
+    src: `https://picsum.photos/500/300?image=${(v + 1) * 5 + 10}`,
+  })),
+  {
+    id: 21,
+    type: 'video/mp4',
+    lazySrc: 'https://picsum.photos/10/6?image=1000',
+    src: 'https://picsum.photos/500/300?image=1000',
+    videoSrc: 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
+  },
+]);
 
 async function api() {
   return new Promise(resolve => {
     setTimeout(() => {
-      resolve(Array.from({ length: 10 }, (k, v) => v + items.value.at(-1) + 1));
+      resolve(
+        Array.from({ length: 10 }, (k, v) => ({
+          id: v + items.value.at(-1).id + 1,
+          type: (v + items.value.at(-1).id + 1) % 5 === 0 ? 'video/mp4' : 'image',
+          lazySrc: `https://picsum.photos/10/6?image=${(v + items.value.at(-1).id + 1) * 5 + 10}`,
+          src: `https://picsum.photos/500/300?image=${(v + items.value.at(-1).id + 1) * 5 + 10}`,
+          ...((v + items.value.at(-1).id + 1) % 5 === 0 && {
+            thumbnail: 'https://picsum.photos/500/300?image=1000',
+            videoSrc: 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
+          }),
+        })),
+      );
     }, 1000);
   });
 }
@@ -50,5 +76,14 @@ function navigateToExternalPath(path: string) {
 .v-col {
   padding: 2px !important; /* 画像同士の間隔を狭める */
   cursor: pointer; /* マウスオーバー時にカーソルをポインターに変更 */
+}
+
+.play-icon {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 60px;
 }
 </style>
