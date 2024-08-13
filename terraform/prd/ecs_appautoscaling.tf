b3b9a0736cbdfc8eaba280ec94d19f45abd13e08
sekijum <Jumpei0910@icloud.com>
2024-08-14 02:35:54 +0900

    fix: remove task

--- terraform/prd/ecs_appautoscaling.tf
+++ terraform/prd/ecs_appautoscaling.tf
@@ -1,133 +1,133 @@
-# ecs server task
-resource "aws_appautoscaling_target" "server" {
-  service_namespace  = "ecs"
-  scalable_dimension = "ecs:service:DesiredCount"
-  resource_id        = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.server.name}"
-  min_capacity       = 1
-  max_capacity       = 3
-}
-resource "aws_appautoscaling_scheduled_action" "ecs_server_task_scale_out" {
-  name               = "${local.app_name}-server-task-scale-out"
-  service_namespace  = aws_appautoscaling_target.server.service_namespace
-  resource_id        = aws_appautoscaling_target.server.resource_id
-  scalable_dimension = aws_appautoscaling_target.server.scalable_dimension
-  timezone           = "Asia/Tokyo"
-  schedule           = "cron(15 10 ? * 6L 2002-2005)"
-  scalable_target_action {
-    min_capacity = 1
-    max_capacity = 3
-  }
-}
-resource "aws_appautoscaling_scheduled_action" "ecs_server_task_scale_in" {
-  name               = "${local.app_name}-server-task-scale-in"
-  service_namespace  = aws_appautoscaling_target.server.service_namespace
-  resource_id        = aws_appautoscaling_target.server.resource_id
-  scalable_dimension = aws_appautoscaling_target.server.scalable_dimension
-  timezone           = "Asia/Tokyo"
-  schedule           = "cron(15 10 ? * 6L 2002-2005)"
-  scalable_target_action {
-    min_capacity = 0
-    max_capacity = 0
-  }
-}
-# cpu使用率を60%に維持
-resource "aws_appautoscaling_policy" "ecs_server_task_cpu_scale" {
-  name               = "${local.app_name}-server-task-cpu-scale"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.server.resource_id
-  scalable_dimension = aws_appautoscaling_target.server.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.server.service_namespace
-  target_tracking_scaling_policy_configuration {
-    target_value       = 60
-    scale_in_cooldown  = 60
-    scale_out_cooldown = 30
+# # ecs server task
+# resource "aws_appautoscaling_target" "server" {
+#   service_namespace  = "ecs"
+#   scalable_dimension = "ecs:service:DesiredCount"
+#   resource_id        = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.server.name}"
+#   min_capacity       = 1
+#   max_capacity       = 3
+# }
+# resource "aws_appautoscaling_scheduled_action" "ecs_server_task_scale_out" {
+#   name               = "${local.app_name}-server-task-scale-out"
+#   service_namespace  = aws_appautoscaling_target.server.service_namespace
+#   resource_id        = aws_appautoscaling_target.server.resource_id
+#   scalable_dimension = aws_appautoscaling_target.server.scalable_dimension
+#   timezone           = "Asia/Tokyo"
+#   schedule           = "cron(15 10 ? * 6L 2002-2005)"
+#   scalable_target_action {
+#     min_capacity = 1
+#     max_capacity = 3
+#   }
+# }
+# resource "aws_appautoscaling_scheduled_action" "ecs_server_task_scale_in" {
+#   name               = "${local.app_name}-server-task-scale-in"
+#   service_namespace  = aws_appautoscaling_target.server.service_namespace
+#   resource_id        = aws_appautoscaling_target.server.resource_id
+#   scalable_dimension = aws_appautoscaling_target.server.scalable_dimension
+#   timezone           = "Asia/Tokyo"
+#   schedule           = "cron(15 10 ? * 6L 2002-2005)"
+#   scalable_target_action {
+#     min_capacity = 0
+#     max_capacity = 0
+#   }
+# }
+# # cpu使用率を60%に維持
+# resource "aws_appautoscaling_policy" "ecs_server_task_cpu_scale" {
+#   name               = "${local.app_name}-server-task-cpu-scale"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.server.resource_id
+#   scalable_dimension = aws_appautoscaling_target.server.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.server.service_namespace
+#   target_tracking_scaling_policy_configuration {
+#     target_value       = 60
+#     scale_in_cooldown  = 60
+#     scale_out_cooldown = 30
 
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageCPUUtilization"
-    }
-  }
-}
-# メモリ使用率を60%に維持
-resource "aws_appautoscaling_policy" "ecs_server_task_memory_scale" {
-  name               = "${local.app_name}-server-task-memory-scale"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.server.resource_id
-  scalable_dimension = aws_appautoscaling_target.server.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.server.service_namespace
-  target_tracking_scaling_policy_configuration {
-    target_value       = 60
-    scale_in_cooldown  = 60
-    scale_out_cooldown = 30
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageCPUUtilization"
+#     }
+#   }
+# }
+# # メモリ使用率を60%に維持
+# resource "aws_appautoscaling_policy" "ecs_server_task_memory_scale" {
+#   name               = "${local.app_name}-server-task-memory-scale"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.server.resource_id
+#   scalable_dimension = aws_appautoscaling_target.server.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.server.service_namespace
+#   target_tracking_scaling_policy_configuration {
+#     target_value       = 60
+#     scale_in_cooldown  = 60
+#     scale_out_cooldown = 30
 
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-    }
-  }
-}
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+#     }
+#   }
+# }
 
-# ecs client task
-resource "aws_appautoscaling_target" "client" {
-  service_namespace  = "ecs"
-  scalable_dimension = "ecs:service:DesiredCount"
-  resource_id        = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.client.name}"
-  min_capacity       = 1
-  max_capacity       = 3
-}
-resource "aws_appautoscaling_scheduled_action" "ecs_client_task_scale_out" {
-  name               = "${local.app_name}-client-task-scale-out"
-  service_namespace  = aws_appautoscaling_target.client.service_namespace
-  resource_id        = aws_appautoscaling_target.client.resource_id
-  scalable_dimension = aws_appautoscaling_target.client.scalable_dimension
-  timezone           = "Asia/Tokyo"
-  schedule           = "cron(15 10 ? * 6L 2002-2005)"
-  scalable_target_action {
-    min_capacity = 1
-    max_capacity = 3
-  }
-}
-resource "aws_appautoscaling_scheduled_action" "ecs_client_task_scale_in" {
-  name               = "${local.app_name}-client-task-scale-in"
-  service_namespace  = aws_appautoscaling_target.client.service_namespace
-  resource_id        = aws_appautoscaling_target.client.resource_id
-  scalable_dimension = aws_appautoscaling_target.client.scalable_dimension
-  timezone           = "Asia/Tokyo"
-  schedule           = "cron(15 10 ? * 6L 2002-2005)"
-  scalable_target_action {
-    min_capacity = 0
-    max_capacity = 0
-  }
-}
-# cpu使用率を60%に維持
-resource "aws_appautoscaling_policy" "ecs_client_task_cpu_scale" {
-  name               = "${local.app_name}-server-task-cpu-scale"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.client.resource_id
-  scalable_dimension = aws_appautoscaling_target.client.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.client.service_namespace
-  target_tracking_scaling_policy_configuration {
-    target_value       = 60
-    scale_in_cooldown  = 60
-    scale_out_cooldown = 30
+# # ecs client task
+# resource "aws_appautoscaling_target" "client" {
+#   service_namespace  = "ecs"
+#   scalable_dimension = "ecs:service:DesiredCount"
+#   resource_id        = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.client.name}"
+#   min_capacity       = 1
+#   max_capacity       = 3
+# }
+# resource "aws_appautoscaling_scheduled_action" "ecs_client_task_scale_out" {
+#   name               = "${local.app_name}-client-task-scale-out"
+#   service_namespace  = aws_appautoscaling_target.client.service_namespace
+#   resource_id        = aws_appautoscaling_target.client.resource_id
+#   scalable_dimension = aws_appautoscaling_target.client.scalable_dimension
+#   timezone           = "Asia/Tokyo"
+#   schedule           = "cron(15 10 ? * 6L 2002-2005)"
+#   scalable_target_action {
+#     min_capacity = 1
+#     max_capacity = 3
+#   }
+# }
+# resource "aws_appautoscaling_scheduled_action" "ecs_client_task_scale_in" {
+#   name               = "${local.app_name}-client-task-scale-in"
+#   service_namespace  = aws_appautoscaling_target.client.service_namespace
+#   resource_id        = aws_appautoscaling_target.client.resource_id
+#   scalable_dimension = aws_appautoscaling_target.client.scalable_dimension
+#   timezone           = "Asia/Tokyo"
+#   schedule           = "cron(15 10 ? * 6L 2002-2005)"
+#   scalable_target_action {
+#     min_capacity = 0
+#     max_capacity = 0
+#   }
+# }
+# # cpu使用率を60%に維持
+# resource "aws_appautoscaling_policy" "ecs_client_task_cpu_scale" {
+#   name               = "${local.app_name}-server-task-cpu-scale"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.client.resource_id
+#   scalable_dimension = aws_appautoscaling_target.client.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.client.service_namespace
+#   target_tracking_scaling_policy_configuration {
+#     target_value       = 60
+#     scale_in_cooldown  = 60
+#     scale_out_cooldown = 30
 
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageCPUUtilization"
-    }
-  }
-}
-# メモリ使用率を60%に維持
-resource "aws_appautoscaling_policy" "ecs_client_task_memory_scale" {
-  name               = "${local.app_name}-client-task-memory-scale"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.client.resource_id
-  scalable_dimension = aws_appautoscaling_target.client.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.client.service_namespace
-  target_tracking_scaling_policy_configuration {
-    target_value       = 60
-    scale_in_cooldown  = 60
-    scale_out_cooldown = 30
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageCPUUtilization"
+#     }
+#   }
+# }
+# # メモリ使用率を60%に維持
+# resource "aws_appautoscaling_policy" "ecs_client_task_memory_scale" {
+#   name               = "${local.app_name}-client-task-memory-scale"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.client.resource_id
+#   scalable_dimension = aws_appautoscaling_target.client.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.client.service_namespace
+#   target_tracking_scaling_policy_configuration {
+#     target_value       = 60
+#     scale_in_cooldown  = 60
+#     scale_out_cooldown = 30
 
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-    }
-  }
-}
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+#     }
+#   }
+# }
--- terraform/prd/ecs_service.tf
+++ terraform/prd/ecs_service.tf
@@ -1,95 +1,95 @@
-resource "aws_ecs_service" "server" {
-  name                               = "server"
-  cluster                            = aws_ecs_cluster.this.id
-  platform_version                   = "LATEST"
-  task_definition                    = aws_ecs_task_definition.server.arn
-  desired_count                      = 1
-  deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent         = 200
-  propagate_tags                     = "SERVICE"
-  launch_type                        = "FARGATE"
-  health_check_grace_period_seconds  = 60
-  enable_execute_command             = true
-  deployment_circuit_breaker {
-    enable   = true
-    rollback = true
-  }
-  network_configuration {
-    assign_public_ip = true
-    subnets = [
-      aws_subnet.public_1a.id,
-      aws_subnet.public_1c.id,
-      aws_subnet.public_1d.id,
-    ]
-    security_groups = [aws_security_group.server.id]
-  }
-  load_balancer {
-    target_group_arn = aws_lb_target_group.server.arn
-    container_name   = "proxy"
-    container_port   = 80
-  }
-}
+# resource "aws_ecs_service" "server" {
+#   name                               = "server"
+#   cluster                            = aws_ecs_cluster.this.id
+#   platform_version                   = "LATEST"
+#   task_definition                    = aws_ecs_task_definition.server.arn
+#   desired_count                      = 1
+#   deployment_minimum_healthy_percent = 50
+#   deployment_maximum_percent         = 200
+#   propagate_tags                     = "SERVICE"
+#   launch_type                        = "FARGATE"
+#   health_check_grace_period_seconds  = 60
+#   enable_execute_command             = true
+#   deployment_circuit_breaker {
+#     enable   = true
+#     rollback = true
+#   }
+#   network_configuration {
+#     assign_public_ip = true
+#     subnets = [
+#       aws_subnet.public_1a.id,
+#       aws_subnet.public_1c.id,
+#       aws_subnet.public_1d.id,
+#     ]
+#     security_groups = [aws_security_group.server.id]
+#   }
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.server.arn
+#     container_name   = "proxy"
+#     container_port   = 80
+#   }
+# }
 
-resource "aws_ecs_service" "client" {
-  name                               = "client"
-  cluster                            = aws_ecs_cluster.this.id
-  platform_version                   = "LATEST"
-  task_definition                    = aws_ecs_task_definition.client.arn
-  desired_count                      = 1
-  deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent         = 200
-  propagate_tags                     = "SERVICE"
-  launch_type                        = "FARGATE"
-  health_check_grace_period_seconds  = 60
-  enable_execute_command             = true
-  deployment_circuit_breaker {
-    enable   = true
-    rollback = true
-  }
-  network_configuration {
-    assign_public_ip = true
-    subnets = [
-      aws_subnet.public_1a.id,
-      aws_subnet.public_1c.id,
-      aws_subnet.public_1d.id,
-    ]
-    security_groups = [aws_security_group.client.id]
-  }
-  load_balancer {
-    target_group_arn = aws_lb_target_group.client.arn
-    container_name   = "proxy"
-    container_port   = 80
-  }
-}
+# resource "aws_ecs_service" "client" {
+#   name                               = "client"
+#   cluster                            = aws_ecs_cluster.this.id
+#   platform_version                   = "LATEST"
+#   task_definition                    = aws_ecs_task_definition.client.arn
+#   desired_count                      = 1
+#   deployment_minimum_healthy_percent = 50
+#   deployment_maximum_percent         = 200
+#   propagate_tags                     = "SERVICE"
+#   launch_type                        = "FARGATE"
+#   health_check_grace_period_seconds  = 60
+#   enable_execute_command             = true
+#   deployment_circuit_breaker {
+#     enable   = true
+#     rollback = true
+#   }
+#   network_configuration {
+#     assign_public_ip = true
+#     subnets = [
+#       aws_subnet.public_1a.id,
+#       aws_subnet.public_1c.id,
+#       aws_subnet.public_1d.id,
+#     ]
+#     security_groups = [aws_security_group.client.id]
+#   }
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.client.arn
+#     container_name   = "proxy"
+#     container_port   = 80
+#   }
+# }
 
-resource "aws_ecs_service" "adminer" {
-  name                               = "adminer"
-  cluster                            = aws_ecs_cluster.this.id
-  platform_version                   = "LATEST"
-  task_definition                    = aws_ecs_task_definition.adminer.arn
-  desired_count                      = 1
-  deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent         = 200
-  propagate_tags                     = "SERVICE"
-  launch_type                        = "FARGATE"
-  health_check_grace_period_seconds  = 60
-  enable_execute_command             = true
-  deployment_circuit_breaker {
-    enable   = true
-    rollback = true
-  }
-  network_configuration {
-    assign_public_ip = true
-    subnets = [
-      aws_subnet.public_1a.id,
-      aws_subnet.public_1c.id,
-      aws_subnet.public_1d.id,
-    ]
-    security_groups = [aws_security_group.adminer.id]
-  }
-  load_balancer {
-    target_group_arn = aws_lb_target_group.adminer.arn
-    container_name   = "adminer"
-    container_port   = 8080
-  }
-}
+# resource "aws_ecs_service" "adminer" {
+#   name                               = "adminer"
+#   cluster                            = aws_ecs_cluster.this.id
+#   platform_version                   = "LATEST"
+#   task_definition                    = aws_ecs_task_definition.adminer.arn
+#   desired_count                      = 1
+#   deployment_minimum_healthy_percent = 50
+#   deployment_maximum_percent         = 200
+#   propagate_tags                     = "SERVICE"
+#   launch_type                        = "FARGATE"
+#   health_check_grace_period_seconds  = 60
+#   enable_execute_command             = true
+#   deployment_circuit_breaker {
+#     enable   = true
+#     rollback = true
+#   }
+#   network_configuration {
+#     assign_public_ip = true
+#     subnets = [
+#       aws_subnet.public_1a.id,
+#       aws_subnet.public_1c.id,
+#       aws_subnet.public_1d.id,
+#     ]
+#     security_groups = [aws_security_group.adminer.id]
+#   }
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.adminer.arn
+#     container_name   = "adminer"
+#     container_port   = 8080
+#   }
+# }
